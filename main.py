03a94da33e888d58d37d6fd31f60ab8ec1dfd4da
2024-045 HwangHyun <24045hyun@ecount.kr>
2025-10-30 22:24:02 +0900

    Path Parameters and Numeric Validations

--- main.py
+++ main.py
@@ -1,46 +1,36 @@
-import random
 from typing import Annotated
 
-from fastapi import FastAPI, Query
+from fastapi import FastAPI, Path, Query
 from pydantic import AfterValidator
 
 app = FastAPI()
 
 
-@app.get("/items/")
+def check_item_id(item_id: int) -> int:
+    if item_id > 1000:
+        raise ValueError("Item ID must be less than 1000")
+    return item_id
+
+
+@app.get("/items2/{item_id}")
+async def read_items2(
+    item_id: Annotated[int, AfterValidator(check_item_id)],
+    q: str | None = None,
+    size: Annotated[float, Query(gt=0, lt=1.0)] = 0.5,
+):
+    results = {"item_id": item_id, "size": size}
+    if q:
+        results.update({"q": q})
+    return results
+
+
+@app.get("/items/{item_id}")
 async def read_items(
-    q: Annotated[
-        list[str] | None,
-        Query(
-            title="Query string",
-            description="Query string for the items to search in the database that have a good match",
-            alias="item-query",
-            deprecated=True,
-            min_length=3,
-        ),
-    ] = None,
-    hidden_query: Annotated[str, Query(include_in_schema=False)] = None,
-) -> dict:
-    return {"items": q, "hidden_query": hidden_query}
-
-
-data = {
-    "isbn-9781529046137": "The Hitchhiker's Guide to the Galaxy",
-    "imdb-tt0371724": "The Hitchhiker's Guide to the Galaxy",
-    "isbn-9781439512982": "Isaac Asimov: The Complete Stories, Vol. 2",
-}
-
-
-def check_valid_id(id: str):
-    if not id.startswith(("isbn-", "imdb-")):
-        raise ValueError("Invalid ID, it must start with 'isbn-' or 'imdb-'")
-    return id
-
-
-@app.get("/books")
-async def read_books(id: Annotated[str | None, AfterValidator(check_valid_id)] = None):
-    if id:
-        item = data.get(id)
-    else:
-        id, item = random.choice(list(data.items()))
-    return {"id": id, "item": item}
+    item_id: Annotated[int, Path(title="The ID of the item to get", gt=0, le=1000)],
+    q: str | None = None,
+    size: Annotated[float, Query(gt=0, lt=1.0)] = 0.5,
+):
+    results = {"item_id": item_id, "size": size}
+    if q:
+        results.update({"q": q})
+    return results
