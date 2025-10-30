d6c0b36a5eff9d52548afd5b185598f29f6fafc5
2024-045 HwangHyun <24045hyun@ecount.kr>
2025-10-30 17:49:57 +0900

    Query Parameters

--- main.py
+++ main.py
@@ -1,42 +1,38 @@
-from enum import Enum
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items/")
+async def read_item(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip : skip + limit]
 
 
 @app.get("/items/{item_id}")
-async def read_item(item_id: int):
+async def read_item2(item_id: str, q: str | None):
+    if q:
+        return {"item_id": item_id, "q": q}
     return {"item_id": item_id}
 
 
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "current_user"}
+@app.get("/items3/{item_id}")
+async def read_item3(item_id: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {"description": "This is an amazing item that has a long description"}
+        )
+    return item
 
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
-
-
-class ModelName(str, Enum):
-    alexnet = "alexnet"
-    resnet = "resnet"
-    lenet = "lenet"
-
-
-@app.get("/models/{model_name}")
-async def get_model(model_name: ModelName):
-    if model_name == ModelName.alexnet:
-        return {"model_name": model_name, "message": "Deep Learning FTW!"}
-
-    if model_name.value == "lenet":
-        return {"model_name": model_name, "message": "LeCNN all the images"}
-
-    return {"model_name": model_name, "message": "Have some residuals"}
+@app.get("/items4/{item_id}")
+async def read_user_item(
+    item_id: str, needy: str, skip: int = 0, limit: int | None = None
+):
+    item = {"item_id": item_id, "needy": needy, "skip": skip, "limit": limit}
+    return item
