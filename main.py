97ff4b53f40e97efd61bfe4f6951320171c44646
2024-045 HwangHyun <24045hyun@ecount.kr>
2025-10-31 18:56:04 +0900

    Body - Nested Models

--- main.py
+++ main.py
@@ -1,23 +1,40 @@
-from typing import Annotated
-
-from fastapi import Body, FastAPI
-from pydantic import BaseModel, Field
+from fastapi import FastAPI
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
 
+class Image(BaseModel):
+    url: HttpUrl
+    name: str
+
+
 class Item(BaseModel):
     name: str
-    description: Annotated[
-        str | None,
-        Field(default=None, title="The description of the item", max_length=300),
-    ]
-    price: Annotated[
-        float, Field(gt=0, description="The price must be greater than zero")
-    ]
+    description: str | None = None
+    price: float
     tax: float | None = None
+    tags: set[str] = set()
+    images: list[Image] | None = None
 
 
-@app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
-    return {"item_id": item_id, "item": item}
+class Offer(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    items: list[Item]
+
+
+@app.post("/offers/")
+async def create_offer(offer: Offer):
+    return {"offer": offer}
+
+
+@app.post("/images/multiple/")
+async def create_multiple_images(images: list[Image]):
+    return {"images": images}
+
+
+@app.post("/index-weights/")
+async def create_index_weights(weights: dict[int, float]):
+    return {"weights": weights}
