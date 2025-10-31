785ca9b2fbc40c52cdb79a3c0a9b467581cdb4c4
2024-045 HwangHyun <24045hyun@ecount.kr>
2025-10-31 18:30:07 +0900

    Body - Fields

--- main.py
+++ main.py
@@ -1,42 +1,23 @@
 from typing import Annotated
 
-from fastapi import Body, FastAPI, Path
-from pydantic import BaseModel
+from fastapi import Body, FastAPI
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
 class Item(BaseModel):
     name: str
-    description: str | None = None
-    price: float
+    description: Annotated[
+        str | None,
+        Field(default=None, title="The description of the item", max_length=300),
+    ]
+    price: Annotated[
+        float, Field(gt=0, description="The price must be greater than zero")
+    ]
     tax: float | None = None
 
 
-class User(BaseModel):
-    username: str
-    full_name: str | None = None
-
-
-@app.put("/items0/{item_id}")
-async def update_item0(item_id: Annotated[int, Path(ge=0, le=1000)], item: Item):
-    return {"item_id": item_id, "item": item}
-
-
 @app.put("/items/{item_id}")
-async def update_item(
-    item_id: Annotated[int, Path(ge=0, le=1000)], item: Item, user: User
-):
-    return {"item_id": item_id, "item": item, "user": user}
-
-
-@app.put("/items2/{item_id}")
-async def update_item2(
-    item_id: int, item: Item, user: User, importance: Annotated[int, Body(gt=0)]
-):
-    return {"item_id": item_id, "item": item, "user": user, "importance": importance}
-
-
-@app.put("/items3/{item_id}")
-async def update_item3(item_id: int, item: Annotated[Item, Body(embed=True)]):
+async def update_item(item_id: int, item: Annotated[Item, Body(embed=True)]):
     return {"item_id": item_id, "item": item}
