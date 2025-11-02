7e06800db8e066c93a33fa8659267881f622bee6
hyun <ghkdgus29@naver.com>
2025-11-02 21:05:10 +0900

    Request Files

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 from typing import Annotated
-from fastapi import FastAPI, Form
-from pydantic import BaseModel
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-class FormData(BaseModel):
-    model_config = {"extra": "forbid"}
-
-    username: str
-    password: str
+@app.post("/files/")
+async def create_file(file: Annotated[bytes, File()]):
+    return {"file_size": len(file)}
 
 
-@app.post("/login/")
-async def login(data: Annotated[FormData, Form()]):
-    return data
+@app.post("/uploadfile/")
+async def create_upload_file(
+    files: Annotated[
+        list[UploadFile], File(description="Multiple files can be uploaded")
+    ],
+):
+    return {"filenames": [file.filename for file in files]}
+
+
+@app.get("/")
+async def main():
+    content = """
+<body>
+<form action="/files/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+<form action="/uploadfiles/" enctype="multipart/form-data" method="post">
+<input name="files" type="file" multiple>
+<input type="submit">
+</form>
+</body>
+    """
+    return HTMLResponse(content=content)
