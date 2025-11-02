687bf37bd14879a40c5ef09af74d253a6db7ca25
hyun <ghkdgus29@naver.com>
2025-11-02 12:27:29 +0900

    Extra Data Types

--- main.py
+++ main.py
@@ -1,77 +1,28 @@
+from datetime import datetime, timedelta, time
 from typing import Annotated
+from uuid import UUID
 
 from fastapi import Body, FastAPI
-from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
-class Item(BaseModel):
-    name: Annotated[str, Field(examples=["Foo"])]
-    description: Annotated[str | None, Field(examples=["A very nice item"])]
-    price: Annotated[float, Field(examples=[42.0])]
-    tax: Annotated[float | None, Field(examples=[3.2])] = None
-
-    # model_config = {
-    #     "json_schema_extra": {
-    #         "examples": [
-    #             {
-    #                 "name": "Foo",
-    #                 "description": "The Foo fighters",
-    #                 "price": 42.0,
-    #                 "tax": 3.2,
-    #             }
-    #         ]
-    #     }
-    # }
-
-
 @app.put("/items/{item_id}")
-async def update_item(item_id: int, item: Item):
-    return {"item_id": item_id, "item": item}
-
-
-class Item2(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float | None = None
-
-
-@app.put("/items2/{item_id}")
-async def update_item2(
-    item_id: int,
-    item: Annotated[
-        Item2,
-        Body(
-            openapi_examples={
-                "normal": {
-                    "summary": "A normal item",
-                    "description": "A **normal** item works correctly.",
-                    "value": {
-                        "name": "Foo",
-                        "description": "A very nice item",
-                        "price": 42.0,
-                        "tax": 3.2,
-                    },
-                },
-                "converted": {
-                    "summary": "An item with converted data",
-                    "description": "An item with `converted` data works `correctly.`",
-                    "value": {
-                        "name": "Bar",
-                        "price": "42.0",
-                    },
-                },
-                "invalid": {
-                    "summary": "An invalid item",
-                    "value": {
-                        "name": "Baz",
-                        "price": "forty-two",
-                    },
-                },
-            },
-        ),
-    ],
+async def read_items(
+    item_id: UUID,
+    start_datetime: Annotated[datetime, Body()],
+    end_datetime: Annotated[datetime, Body()],
+    process_after: Annotated[timedelta, Body()],
+    repeat_at: Annotated[time | None, Body()] = None,
 ):
-    return {"item_id": item_id, "item": item}
+    start_process = start_datetime + process_after
+    duration = end_datetime - start_process
+    return {
+        "item_id": item_id,
+        "start_datetime": start_datetime,
+        "end_datetime": end_datetime,
+        "process_after": process_after,
+        "repeat_at": repeat_at,
+        "start_process": start_process,
+        "duration": duration,
+    }
