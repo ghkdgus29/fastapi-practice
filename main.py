adeb9d2f2156827bed0b4879adb369b004d965f7
hyun <ghkdgus29@naver.com>
2025-11-02 15:30:19 +0900

    Form Data

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI, status
+from typing import Annotated
+from fastapi import FastAPI, Form
 
 app = FastAPI()
 
 
-@app.post("/items/", status_code=status.HTTP_201_CREATED)
-async def create_item(name: str) -> dict:
-    return {"name": name, "message": "Item created successfully"}
+@app.post("/login/")
+async def login(username: Annotated[str, Form()], password: Annotated[str, Form()]):
+    return {"username": username}
