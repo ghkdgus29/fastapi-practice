49fe1bd4e8f91878fb039995ffd4916798793084
2024-045 HwangHyun <24045hyun@ecount.kr>
2025-11-05 14:20:54 +0900

    Classes as Dependencies

--- main.py
+++ main.py
@@ -4,31 +4,31 @@ from fastapi import Depends, FastAPI
 
 app = FastAPI()
 
+fake_items_db = [{"item_name": "Foo"}, {"item_name": "Bar"}, {"item_name": "Baz"}]
 
-async def common_parameters(
-    q: str | None = None, skip: int = 0, limit: int = 100
-) -> dict:
-    return {"q": q, "skip": skip, "limit": limit}
+
+class CommonQueryParams:
+    def __init__(self, q: str | None = None, skip: int = 0, limit: int = 100):
+        self.q = q
+        self.skip = skip
+        self.limit = limit
 
 
 @app.get("/items/")
-async def read_items(commons: Annotated[dict, Depends(common_parameters)]):
-    return commons
-
-
-@app.get("/users/")
-async def read_users(commons: Annotated[dict, Depends(common_parameters)]):
-    return commons
-
-
-CommonsDep = Annotated[dict, Depends(common_parameters)]
+async def read_items(commons: Annotated[CommonQueryParams, Depends(CommonQueryParams)]):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
 
 
 @app.get("/items2/")
-async def read_items2(commons: CommonsDep):
-    return commons
-
-
-@app.get("/users2/")
-async def read_users2(commons: CommonsDep):
-    return commons
+async def read_items2(commons: Annotated[CommonQueryParams, Depends()]):
+    response = {}
+    if commons.q:
+        response.update({"q": commons.q})
+    items = fake_items_db[commons.skip : commons.skip + commons.limit]
+    response.update({"items": items})
+    return response
