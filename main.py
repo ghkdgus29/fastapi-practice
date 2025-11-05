17163b3b69605c4a8ee3520b2c4e1fa4356cec2a
ghkkdgus29 <ghkdgus29@naver.com>
2025-11-05 15:32:30 +0900

    Global Dependencies

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from typing import Annotated
 
 from fastapi import Depends, FastAPI, Header, HTTPException
 
-app = FastAPI()
-
 
 async def verify_token(x_token: Annotated[str, Header()]):
     if x_token != "fake-super-secret-token":
@@ -18,6 +16,14 @@ async def verify_key(x_key: Annotated[str, Header()]):
     return x_key
 
 
-@app.get("/items/", dependencies=[Depends(verify_token), Depends(verify_key)])
+app = FastAPI(dependencies=[Depends(verify_token), Depends(verify_key)])
+
+
+@app.get("/items/")
 async def read_items():
     return [{"item": "Foo"}, {"item": "Bar"}]
+
+
+@app.get("/users/")
+async def read_users():
+    return [{"user": "John"}, {"user": "Jane"}]
