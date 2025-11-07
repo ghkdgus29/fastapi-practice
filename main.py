e75e257c8ac1f311284b08c1b8360e27043b849c
ghkkdgus29 <ghkdgus29@naver.com>
2025-11-07 10:03:10 +0900

    Metadata and Docs URLs

--- main.py
+++ main.py
@@ -1,28 +1,38 @@
-from typing import Annotated
+from fastapi import FastAPI
 
-from fastapi import BackgroundTasks, Depends, FastAPI
+description = """
+ChimichangApp API helps you do awesome stuff. 🚀
 
-app = FastAPI()
+## Items
+
+You can **read items**.
+
+## Users
+
+You will be able to:
+
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
+
+app = FastAPI(
+    title="ChimichangApp",
+    description=description,
+    summary="Deadpool's favorite app. Nuff said.",
+    version="0.0.1",
+    terms_of_service="http://example.com/terms/",
+    contact={
+        "name": "Deadpoolio the Amazing",
+        "url": "http://x-force.example.com/contact/",
+        "email": "dp@x-force.example.com",
+    },
+    license_info={
+        "name": "Apache 2.0",
+        "identifier": "MIT",
+    },
+)
 
 
-def write_log(message: str):
-    with open("log.txt", mode="a") as log:
-        log.write(message)
-
-
-def get_query(background_tasks: BackgroundTasks, q: str | None = None):
-    if q:
-        message = f"found query: {q}\n"
-        background_tasks.add_task(write_log, message)
-    return q
-
-
-@app.post("/send-notification/{email}")
-async def send_notification(
-    email: str,
-    background_tasks: BackgroundTasks,
-    q: Annotated[str | None, Depends(get_query)],
-):
-    message = f"message to {email}"
-    background_tasks.add_task(write_log, message)
-    return {"message": "Message sent"}
+@app.get("/items/")
+async def read_items():
+    return [{"name": "Katana"}]
