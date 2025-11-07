60e9054af1d05872bc9ddc1463a94112a6bb1fde
ghkkdgus29 <ghkdgus29@naver.com>
2025-11-07 19:49:24 +0900

    Additional Responses in OpenAPI

--- main.py
+++ main.py
@@ -1,93 +1,79 @@
-from typing import Any
+from fastapi import FastAPI
+from fastapi.responses import FileResponse, JSONResponse
+from pydantic import BaseModel
 
-import orjson
-from fastapi import FastAPI, Response
-from fastapi.responses import (
-    FileResponse,
-    HTMLResponse,
-    ORJSONResponse,
-    PlainTextResponse,
-    RedirectResponse,
-    StreamingResponse,
-)
+
+class Item(BaseModel):
+    id: str
+    value: str
+
+
+class Message(BaseModel):
+    message: str
+
 
 app = FastAPI()
 
 
-@app.get("/items/", response_class=ORJSONResponse)
-async def read_items():
-    return ORJSONResponse([{"items": "Foo"}])
+@app.get("/items/{item_id}", response_model=Item, responses={404: {"model": Message}})
+async def read_item(item_id: str):
+    if item_id == "foo":
+        return {"id": "foo", "value": "there goes my hero"}
+    return JSONResponse(status_code=404, content={"message": "Item not found"})
 
 
-@app.get("/items2/")
-async def read_items2():
-    html_content = """
-    <html>
-        <head>
-            <title>Some HTML in here</title>
-        </head>
-        <body>
-            <h1>Look ma! HTML!</h1>
-        </body>
-    </html>
-    """
-
-    return HTMLResponse(content=html_content, status_code=200)
-
-
-@app.get("/legacy/")
-def get_legacy_data():
-    data = """<?xml version="1.0"?>
-    <shampoo>
-    <Header>
-        Apply shampoo here.
-    </Header>
-    <Body>
-        You'll have to use soap here.
-    </Body>
-    </shampoo>
-    """
-    return Response(content=data, media_type="application/xml")
+@app.get(
+    "/items2/{item_id}",
+    response_model=Item,
+    responses={
+        200: {
+            "content": {"image/png": {}},
+            "description": "Return the JSON item or an image",
+        }
+    },
+)
+async def read_item2(item_id: str, img: bool | None = None):
+    if img:
+        return FileResponse("image.png", media_type="image/png")
+    else:
+        return {"id": "foo", "value": "there goes my hero"}
 
 
-@app.get("/", response_class=PlainTextResponse)
-async def main():
-    return "Hello, World!"
+@app.get(
+    "/items3/{item_id}",
+    response_model=Item,
+    responses={
+        404: {"model": Message, "description": "The item was not found"},
+        200: {
+            "description": "Item requested by ID",
+            "content": {
+                "application/json": {
+                    "example": {"id": "bar", "value": "The bartenders"}
+                }
+            },
+        },
+    },
+)
+async def read_item3(item_id: str):
+    if item_id == "foo":
+        return {"id": "foo", "value": "there goes my hero"}
+    return JSONResponse(status_code=404, content={"message": "Item not found"})
 
 
-@app.get("/typer")
-async def redirct_typer():
-    return RedirectResponse("https://typer.tiangolo.com/")
+responses = {
+    404: {"description": "Item not found"},
+    302: {"description": "The item was moved"},
+    303: {"description": "Not enough privileges"},
+}
 
 
-@app.get("/pydantic", response_class=RedirectResponse, status_code=302)
-async def redirect_pydantic():
-    return "https://docs.pydantic.dev/"
-
-
-async def fake_video_streamer():
-    for i in range(10):
-        yield b"some fake video bytes"
-
-
-@app.get("/stream")
-async def stream_video():
-    return StreamingResponse(fake_video_streamer())
-
-
-@app.get("/file", response_class=FileResponse)
-async def read_file():
-    return FileResponse("log.txt")
-
-
-class CustomORJSONResponse(Response):
-    media_type = "application/json"
-
-    def render(self, content: Any) -> bytes:
-        assert orjson is not None, "orjson must be installed to use this response class"
-        return orjson.dumps(content, option=orjson.OPT_INDENT_2)
-
-
-@app.get("/custom-orjson", response_class=CustomORJSONResponse)
-async def custom_orjson():
-    return {"message": "Hello, World!"}
+@app.get(
+    "/items4/{item_id}",
+    response_model=Item,
+    responses={**responses, 200: {"content": {"image/png": {}}}},
+)
+async def read_item4(item_id: str, img: bool | None = None):
+    if img:
+        return FileResponse("image.png", media_type="image/png")
+    else:
+        return {"id": "foo", "value": "there goes my hero"}
