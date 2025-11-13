54930c62d1727fcf002f3464bba1817b87cec23a
ghkkdgus29 <ghkdgus29@naver.com>
2025-11-13 12:58:22 +0900

    Sub Applications - Mounts

--- main.py
+++ main.py
@@ -1,25 +1,19 @@
 from fastapi import FastAPI
 
-# from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
-# from fastapi.middleware.trustedhost import TrustedHostMiddleware
-from fastapi.middleware.gzip import GZipMiddleware
-
 app = FastAPI()
 
 
-# app.add_middleware(HTTPSRedirectMiddleware)  # http -> https / ws -> wss redirect
-# app.add_middleware(
-#     TrustedHostMiddleware,
-#     allowed_hosts=[
-#         "example.com",
-#         "*.example.com",
-#     ],
-# )  # allowed hosts to access the server
-app.add_middleware(
-    GZipMiddleware, minimum_size=1000, compresslevel=5
-)  # if request includes Accept-Encoding: gzip, then gzip the response
+@app.get("/app")
+def read_main():
+    return {"message": "Hello World from main app"}
 
 
-@app.get("/")
-async def main():
-    return {"message": "Hello World"}
+subapi = FastAPI()
+
+
+@subapi.get("/sub")
+def read_sub():
+    return {"message": "Hello World from sub api"}
+
+
+app.mount("/subapi", subapi)
