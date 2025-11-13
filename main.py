f581dfb1eae1d2d8cbab6e7866d9a41e78bca484
ghkkdgus29 <ghkdgus29@naver.com>
2025-11-13 11:36:39 +0900

    Using Dataclasses

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
+from dataclasses import dataclass, field
+
 from fastapi import FastAPI
-from fastapi.requests import Request
+
+
+@dataclass
+class Item:
+    name: str
+    price: float
+    tags: list[str] = field(default_factory=list)
+    description: str | None = None
+    tax: float | None = None
+
 
 app = FastAPI()
 
 
-@app.get("/items/{item_id}")
-def read_root(item_id: str, request: Request):
-    client_host = request.client.host
-    return {"item_id": item_id, "client_host": client_host}
+@app.get("/items/next", response_model=Item)
+async def read_next_item():
+    return {
+        "name": "Island In The Moon",
+        "price": 12.99,
+        "description": "A place to be playin' and havin' fun",
+        "tags": ["breater"],
+    }
