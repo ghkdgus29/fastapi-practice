14d5934c23d5219a692e32bd92224c7604b9c182
ghkkdgus29 <ghkdgus29@naver.com>
2025-11-13 12:52:32 +0900

    Advanced Middleware

--- main.py
+++ main.py
@@ -1,25 +1,25 @@
-from dataclasses import dataclass, field
-
 from fastapi import FastAPI
 
-
-@dataclass
-class Item:
-    name: str
-    price: float
-    tags: list[str] = field(default_factory=list)
-    description: str | None = None
-    tax: float | None = None
-
+# from fastapi.middleware.httpsredirect import HTTPSRedirectMiddleware
+# from fastapi.middleware.trustedhost import TrustedHostMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 
 app = FastAPI()
 
 
-@app.get("/items/next", response_model=Item)
-async def read_next_item():
-    return {
-        "name": "Island In The Moon",
-        "price": 12.99,
-        "description": "A place to be playin' and havin' fun",
-        "tags": ["breater"],
-    }
+# app.add_middleware(HTTPSRedirectMiddleware)  # http -> https / ws -> wss redirect
+# app.add_middleware(
+#     TrustedHostMiddleware,
+#     allowed_hosts=[
+#         "example.com",
+#         "*.example.com",
+#     ],
+# )  # allowed hosts to access the server
+app.add_middleware(
+    GZipMiddleware, minimum_size=1000, compresslevel=5
+)  # if request includes Accept-Encoding: gzip, then gzip the response
+
+
+@app.get("/")
+async def main():
+    return {"message": "Hello World"}
