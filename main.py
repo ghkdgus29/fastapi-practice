9fc1d236a309fef6253f789cbe14b1cbce4a7deb
ghkkdgus29 <ghkdgus29@naver.com>
2025-11-13 14:47:40 +0900

    Templates

--- main.py
+++ main.py
@@ -1,19 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/app")
-def read_main():
-    return {"message": "Hello World from main app"}
-
-
-subapi = FastAPI()
-
-
-@subapi.get("/sub")
-def read_sub():
-    return {"message": "Hello World from sub api"}
-
-
-app.mount("/subapi", subapi)
+@app.get("/items/{id}", response_class=HTMLResponse)
+async def read_item(request: Request, id: str):
+    return templates.TemplateResponse(
+        name="item.html", request=request, context={"id": id}
+    )
