6152f94329d3c927b3e174dc505ba6d0428518a0
hyun <ghkdgus29@naver.com>
2025-11-15 18:11:51 +0900

    OpenAPI Callbacks

--- main.py
+++ main.py
@@ -1,21 +1,51 @@
-from typing import Annotated
-from fastapi import Depends, FastAPI
-from config import Settings
-from functools import lru_cache
-
+from fastapi import APIRouter, FastAPI
+from pydantic import BaseModel, HttpUrl
 
 app = FastAPI()
 
 
-@lru_cache
-def get_settings():
-    return Settings()
+class Invoice(BaseModel):
+    id: str
+    title: str | None = None
+    customer: str
+    total: float
 
 
-@app.get("/info")
-async def info(settings: Annotated[Settings, Depends(get_settings)]):
-    return {
-        "app_name": settings.app_name,
-        "admin_email": settings.admin_email,
-        "items_per_user": settings.items_per_user,
-    }
+class InvoiceEvent(BaseModel):
+    description: str
+    paid: bool
+
+
+class InvoiceEventReceived(BaseModel):
+    ok: bool
+
+
+invoices_callback_router = APIRouter()
+
+
+@invoices_callback_router.post(
+    "{$callback_url}/invoices/{$request.body.id}", response_model=InvoiceEventReceived
+)
+def invoice_notification(body: InvoiceEvent):
+    pass
+
+
+@app.post("/invoices/", callbacks=invoices_callback_router.routes)
+def create_invoice(invoice: Invoice, callback_url: HttpUrl | None = None):
+    """
+    Create an invoice.
+
+    This will (let's imagine) let the API user (some external developer) create an
+    invoice.
+
+    And this path operation will:
+
+    * Send the invoice to the client.
+    * Collect the money from the client.
+    * Send a notification back to the API user (the external developer), as a callback.
+        * At this point is that the API will somehow send a POST request to the
+            external API with the notification of the invoice event
+            (e.g. "payment successful").
+    """
+    # Send the invoice, collect the money, send the notification (the callback)
+    return {"msg": "Invoice received"}
